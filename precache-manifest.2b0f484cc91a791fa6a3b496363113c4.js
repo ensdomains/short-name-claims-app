bcfa9be541a2fbad2fc6004c3e18523e3184c844
Nick Johnson <arachnid@notdot.net>
2019-05-28 16:20:11 +1200

    Updates

---
+++ precache-manifest.2b0f484cc91a791fa6a3b496363113c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672572d7ae6b65e84f312ed82334afa7",
+    "url": "/short-name-claims-app/index.html"
+  },
+  {
+    "revision": "760006b504303beff002",
+    "url": "/short-name-claims-app/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2169c06d43c715b781a1",
+    "url": "/short-name-claims-app/static/js/2.cf93da3e.chunk.js"
+  },
+  {
+    "revision": "760006b504303beff002",
+    "url": "/short-name-claims-app/static/js/main.218869b7.chunk.js"
+  },
+  {
+    "revision": "6c13eec2c5bac037b9e5",
+    "url": "/short-name-claims-app/static/js/runtime~main.7dc517f2.js"
+  }
+]);

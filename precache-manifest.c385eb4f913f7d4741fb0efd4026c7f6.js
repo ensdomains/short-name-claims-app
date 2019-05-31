5c822df68c496f5cfe101bf4596fe2627ed7468a
Nick Johnson <arachnid@notdot.net>
2019-05-31 15:28:52 +1200

    Updates

---
+++ precache-manifest.c385eb4f913f7d4741fb0efd4026c7f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a97f01874eb81ff0335c1ad2bafc4dc",
+    "url": "/short-name-claims-app/index.html"
+  },
+  {
+    "revision": "ecf4edad454cd5a91607",
+    "url": "/short-name-claims-app/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "bafd895888f29503e655",
+    "url": "/short-name-claims-app/static/js/2.7fa04406.chunk.js"
+  },
+  {
+    "revision": "ecf4edad454cd5a91607",
+    "url": "/short-name-claims-app/static/js/main.f96980d0.chunk.js"
+  },
+  {
+    "revision": "6c13eec2c5bac037b9e5",
+    "url": "/short-name-claims-app/static/js/runtime~main.7dc517f2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/short-name-claims-app/precache-manifest.947f79f76e7c02b54e6a7386584630e8.js"
+  "/short-name-claims-app/precache-manifest.c385eb4f913f7d4741fb0efd4026c7f6.js"
 );
 
 self.addEventListener('message', (event) => {

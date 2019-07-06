0c184ff3244baf5ea99aec8cc4e4fe85df985a3d
Nick Johnson <arachnid@notdot.net>
2019-07-06 16:47:12 +1200

    Updates

---
+++ precache-manifest.10f1767fa104be355d907a8b5de64a7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ddda28e4951ca2e3f60826a3a6d522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2028651dd816de9b1aab",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "70730c6378caa191429f",
+    "url": "/static/js/2.a1a7d52b.chunk.js"
+  },
+  {
+    "revision": "2028651dd816de9b1aab",
+    "url": "/static/js/main.0ca60c82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.640a1bf26396f93f8013d8a96c89d960.js"
+  "/precache-manifest.10f1767fa104be355d907a8b5de64a7f.js"
 );
 
 self.addEventListener('message', (event) => {
